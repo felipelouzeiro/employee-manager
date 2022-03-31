98e7aa42a8394e3dc335069ba66f0f40931932b1
felipe <fl.louzeiro@gmail.com>
2022-03-31 19:42:56 -0300

    feat: implementa validação das informaçoes do employee antes da sua  criação

--- src/api/services/employee.js
+++ src/api/services/employee.js
@@ -1,6 +1,16 @@
+const { employeeSchema } = require("../../utils/schemas/schemas");
 const { Employee } = require("../models");
 
 const create = async ({ name, email, department, salary, birth_date, password }) => {
+  try {
+    await employeeSchema.validate({ name, email, department, salary, birth_date, password })
+  } catch (error) {
+    if (error) { throw handlingError(404, error.errors[0]); }
+  }
+
+  const alreadyEmployee = await Employee.findOne({ where: { email } });
+
+  if (alreadyEmployee) { throw handlingError(409, 'Employee already registered'); }
 
   const response = await Employee.create({ name, email, department, salary, birth_date, password });
   return response.dataValues;
