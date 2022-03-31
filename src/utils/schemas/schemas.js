f42c188c7048f00282672c05fb5d7b1276a75a5d
felipe <fl.louzeiro@gmail.com>
2022-03-31 19:40:51 -0300

    feat: instala dependencia yup e cria schema para validaçao de informaçoes do employee

---
+++ src/utils/schemas/schemas.js
@@ -0,0 +1,22 @@
+const yup = require('yup');
+
+const employeeSchema = yup.object({
+  name: yup.string()
+  .required('Name is required.'),
+  email: yup.string()
+  .email('Invalid e-mail.')
+  .required('E-mail is required.'),
+  department: yup.string()
+  .required('Department is required.'),
+  salary: yup.number()
+  .required('Salary is required.'),
+  birth_date: yup.string()
+  .required('Birth_date is required.'),
+  password: yup.string()
+  .min(6, 'Password must be 6 or more characters.')
+  .required('Password is required.')
+});
+
+module.exports = {
+  employeeSchema,
+}
